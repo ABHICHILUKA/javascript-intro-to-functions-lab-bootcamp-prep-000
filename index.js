45659a2d0978c9add28de08916ec7111a958bf64
Nikhil Chiluka <chnikhil058@gmail.com>
2018-02-21 07:52:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function whisper(String){
   return String.toLowerCase()
 }
 function logShout(String){
-  var c = String
-  if(c.toUpperCase() === c)
-   console.log('${c}')
+   
+   console.log(String.toUpperCase())
 }
